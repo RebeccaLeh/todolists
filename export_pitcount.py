63c6c152fbf781309314cca19e8c552898de095d
Rebecca Lehman <41304112+RebeccaLeh@users.noreply.github.com>
2018-11-21 13:26:10 -0800

    11/21

--- export_pitcount.py
+++ export_pitcount.py
@@ -77,7 +77,202 @@ class ExcelOutput(object):
             for row in cursor:
                 dataList.append(row)
 
-                for tip of leaf:
+unsheltered = ['Vehicle', 'UnderBridge_Overpass', 'Street', 'Park', 'Outdoors', 'BustStation_Airport', 'AbandonedBuilding']
+        
+        
+        ##################using Pandas get each type of household into a seprate list 
+import pandas as df
+panda = df.DataFrame(dataList)
+#Emergency Shelter
+dfES = panda[panda[11]=='Emergency Shelter']
+#Transitional Housing
+dfTH = panda[panda[11] == 'Transitional Housing']
+#Safe Haven
+dfSH = panda[panda[11] == 'Safe Haven']
+#Unsheltered
+dfUN = panda[panda[11].isin(unsheltered)]
+
+
+        
+#Unique Households and seperate them into families and single 
+family = df.DataFrame()
+single = df.DataFrame()
+HH =  panda[58].unique()
+for ID in HH:
+    HHSep = panda.loc[panda[58] == ID]
+    if len(HHSep)>1:
+        family = family.append(HHSep)
+    else:
+        single = single.append(HHSep)
+    
+#Get Family Type
+
+#########seprate sub groups 
+U18Fam = family[family[13]<18]
+#YouthFam = family[family[13].between(17,25, inclusive=False)] # don't need this yet, using same variable for all youth
+YTHFam = family[family[13]<25]
+ADYFam = family[family[13]>17]
+ADFam = family[family[13]>24]
+
+########do the same for singles
+ADSing = single[single[13]>17]
+CHSing = single[single[13]<18]
+YTSing = single[single[13]<24]
+
+
+
+
+        ############################################get households with at least one adult and one child                                            116
+        ##through this we also get HH with prenting youth
+        ADCH = df.DataFrame()
+        CHFAM = df.DataFrame()
+        ADNOCH = df.DataFrame()
+        for ID in HH:
+            ch = U18Fam.loc[U18Fam[58] == ID]
+            ad = ADYFam.loc[ADYFam[58] == ID]
+            if len(ad.index)>0 and len(ch.index)>0:
+                ADCH = ADCH.append(ad)
+                ADCH = ADCH.append(ch)
+            elif len(ad.index)==0 and len(ch.index)>0:
+                CHFAM = CHFAM.append(ch)
+            elif len(ad.index)>0 and len(ch.index)==0:
+                ADNOCH = ADNOCH.append(ad)
+        print(len(ADCH.index))
+        ##shelter type
+        ###HH
+        ##Individuals
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status
+         
+        ############################################get households with no children                                                      295
+        #Unique Households and seperate them into families and single 
+        #ADNOCH #families with no children
+        #ADSing # single adults
+        noChildren = ADNOCH.append(ADSing)
+        print(len(noChildren.index))
+        ##shelter type
+        ###HH
+        ##Individuals
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status
+
+
+        ##########################################Get households with only children                                                     0
+
+        #families of children
+        #CHFAM ###done
+        #single children
+        #CHSing
+        childOnly = CHFAM.append(CHSing)
+        print(len(childOnly.index))
+        ##shelter type
+        ###HH
+        ##Individuals
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status
+        
+        ############################################Veterans
+        #households with children
+        ##shelter type
+        ###HH
+        ##Individuals
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status 
+
+        ############################################veterans households without children
+       
+        ##shelter type
+        ###HH
+        ##Individuals
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status
+
+        ############################################youth unaccompanied                                                         47
+        UNYTH = df.DataFrame()
+        for ID in HH:
+            yt = YTHFam.loc[YTHFam[58] == ID]
+            ad = ADFam.loc[ADFam[58] == ID]
+            if len(yt.index)>0 and len(ad.index)==0:
+                UNYTH = UNYTH.append(yt)
+
+        YouthUnaccompanied = UNYTH.append(YTSing)
+        print(len(YouthUnaccompanied.index))
+        ####must add singles
+        #ppl under 24
+        ##shelter type
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status
+
+        ############################################Parenting youth                                                         14
+        ##done name is CHFAM
+        print(len(UNYTH.index)) 
+        ####18-24
+        #########Parents
+        #########Children
+        ####under 18
+        #########Parents
+        #########Children
+        #########Parent Demographics below:
+        ####Demographics
+        #####age
+        #####gender
+        #####ethnicity
+        #####race
+        #####chronic homelessness status
+        
+        ##############################################Adult aids, mental illness etc.
+        workbook = xlsxwriter.Workbook(PITCount_Summary)
+        worksheet = workbook.add_worksheet()
+
+        for item, cost in (expense):
+            worksheet.write(row,col,    )
+            worksheet.write(row, col+1, cost)
+            row +=1
+        
+        workbook.close()
+
+      
+        return
+
+
+
+    #############################scratch
+
+            #get list of unique family names 
+        for listing in family:
+            if listing = 1
+                if age >17 single adult
+                if age<18 to 24  
+            if listing >1
+                if age>17 and age <17 fam with child    
+                if arg<17and age <17 child with child  
+
+
+                    for tip of leaf:
                     for leaf in branch
                         for branch in tree   tip for branch in tree for leaf in branch for tip in branch
                 ####strip none as needed 
@@ -102,170 +297,3 @@ class ExcelOutput(object):
                 row
 
         ages = [row for x, x in enumerate(row) if  for row in dataList]
-
-unsheltered = ['Vehicle', 'UnderBridge_Overpass', 'Street', 'Park', 'Outdoors', 'BustStation_Airport', 'AbandonedBuilding']
-        
-        
-        ##################using Pandas get each type of household into a seprate list 
-import pandas as df
-panda = df.DataFrame(dataList)
-#Emergency Shelter
-dfES = panda[panda[11]=='Emergency Shelter']
-#Transitional Housing
-dfTH = panda[panda[11] == 'Transitional Housing']
-#Safe Haven
-dfSH = panda[panda[11] == 'Safe Haven']
-#Unsheltered
-dfUN = panda[panda[11].isin(unsheltered)]
-
-
-        
-#Unique Households and seperate them into families and single 
-HH =  panda[58].unique()
-for ID in HH:
-    HHSep = panda.loc[panda[58] == ID]
-    if len(HHSep)>1:
-        family = family.append(HHSep)
-    else:
-        single = single.append(HHSep)
-    
-#Get Family Type
-
-
-U18Fam = family[family[13]<18]
-YouthFam = family[family[13].between(17,25, inclusive=False)]
-
-
-ADFam = family[family[13]>24]
-
-
-
-
-        #get list of unique family names 
-        for listing in family:
-            if listing = 1
-                if age >17 single adult
-                if age<18 to 24  
-            if listing >1
-                if age>17 and age <17 fam with child    
-                if arg<17and age <17 child with child  
-
-
-    
-    HHSep.iloc[HHSep[13]<18]
-        if len(HHSep)>1:
-            pd.concat(family,HHSep)
-        else:
-            pd.concat(single,HHSep)
-
-    
-
-
-
-
-        #get households with at least one adult and one child
-        dfADCH = panda[panda[13]]
-        ##shelter type
-        ###HH
-        ##Individuals
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status
-         
-        #get households with no children
-        #Unique Households and seperate them into families and single 
-HH =  panda[58].unique()
-for ID in HH:
-    HHSep = panda.loc[panda[58] == ID]
-    if HHSep[HHSep[13]>17]:
-        houseNOCH = houseNOH.append(HHSep)
-
-HH =  panda[58].unique()
-for ID in HH:
-    HHSep = panda.loc[panda[13] > 17]
-    houseNOCH = houseNOH.append(HHSep)
-        ##shelter type
-        ###HH
-        ##Individuals
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status
-
-        #Get households with only children
-        ##shelter type
-        ###HH
-        ##Individuals
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status
-        
-        #Veterans
-        #households with children
-        ##shelter type
-        ###HH
-        ##Individuals
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status 
-
-        #households without children
-        ##shelter type
-        ###HH
-        ##Individuals
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status
-
-        #youth 
-        #ppl under 24
-        ##shelter type
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status
-
-        #Parenting youth
-        ####18-24
-        #########Parents
-        #########Children
-        ####under 18
-        #########Parents
-        #########Children
-        #########Parent Demographics below:
-        ####Demographics
-        #####age
-        #####gender
-        #####ethnicity
-        #####race
-        #####chronic homelessness status
-        
-        ###Adult aids, mental illness etc.
-        workbook = xlsxwriter.Workbook(PITCount_Summary)
-        worksheet = workbook.add_worksheet()
-
-        for item, cost in (expense):
-            worksheet.write(row,col,    )
-            worksheet.write(row, col+1, cost)
-            row +=1
-        
-        workbook.close()
-
-      
-        return
